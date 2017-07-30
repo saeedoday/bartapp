ef7e4a3a887fc4b00f20aa233c90d2773c007889
Saeed Oday <saeedoday@gmail.com>
2017-07-29 21:11:30 -0700

    Initial upload
    Shit works....now I can go back if I break shit.

---
+++ script.js
@@ -0,0 +1,32 @@
+// Declare abbreviation variable
+var stationAbbr;
+
+// Get station code json data
+function getStationCode(callback) {
+  $.getJSON('/stations.json', function (data) {
+    callback(data);
+  });
+}
+
+// Just testing station code data
+getStationCode(function (data) {
+  console.log(data.stationCodes[1]);
+});
+
+// Setup : get train estimate data
+function getEstimate(callback) {
+  $.getJSON('https://api.bart.gov/api/etd.aspx?cmd=etd&orig=' + stationAbbr + '&key=MW9S-E7SL-26DU-VV8V&json=y', function (data) {
+    callback(data);
+  });
+}
+
+// Use abbr variable to get estimate data for specified station
+function testFunction() {
+  stationAbbr = 'embr';
+  console.log(stationAbbr);
+
+  getEstimate(function (estimate, stationAbbr) {
+    console.log(estimate.root.station[0].etd);
+    alert("Thing has happened");
+  });
+}
